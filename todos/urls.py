52d7eff45500b84ca16e5959cb9e9ff4ab843f0e
1asset <82859085+1asset@users.noreply.github.com>
2022-01-21 02:45:04 +0600

    Update urls.py

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from .views import TaskCreate, TaskList, TaskDetail, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
 
-urlpatterns =[
+urlpatterns  =[
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
     path('list/',views.list_todo_items),
     path('insert_todo/',views.insert_todo_item,name='insert_todo_item'),
     path('delete_todo/<int:todo_id>/',views.delete_todo_item,name='delete_todo_item'),
